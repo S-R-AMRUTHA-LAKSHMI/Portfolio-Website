378e826c440c3792f7d6cad9dfb957816b6e049d
AMRUTHA LAKSHMI S R <129605733+S-R-AMRUTHA-LAKSHMI@users.noreply.github.com>
2024-07-05 19:33:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Frontend Developer","Backend Developer","Full Stack Developer","AI Ensiasist"],
+    typeSpeed: 120,
+    backSpeed: 120,
+    backDelay: 1000,
+    loop: true
+})
